d1c795c243e85621e21b35d7d1cffa28e1d26cdb
antoniodi <antonio4793@hotmail.com>
2021-01-30 18:22:07 -0500

    improve code for validate user

--- app/co/com/application/services/UserManagerService.scala
+++ app/co/com/application/services/UserManagerService.scala
@@ -1,21 +1,18 @@
 package co.com.application.services
 
-import cats.data.{ EitherT, Reader }
+import cats.data.{EitherT, Reader}
 import co.com.application.acl.dtos.UserDTO
 import co.com.application.controllers.commands.Dependency
 import co.com.infrastructure.Types.EitherTResult
 import monix.eval.Task
-import co.com.application.acl.dtos.UserDTO._
 
 trait UserManagerService {
 
   def findUser( username: String ): Reader[Dependency, EitherTResult[UserDTO]] = Reader {
     dependecy: Dependency =>
 
-      for {
-        user <- dependecy.userRepo.find( username ).run( dependecy.dbConfig )
-        validUser <- EitherT( Task( dependecy.userService.validateUser( user ) ) )
-      } yield validUser
+      dependecy.userRepo.find( username ).run( dependecy.dbConfig )
+        .flatMap(user => EitherT( Task( dependecy.userService.validateUser( user ) ) ) )
   }
 
 }
