67eae2c482e42701a529464c7ba623f84e93888e
antoniodi <antonio4793@hotmail.com>
2021-06-07 20:01:38 -0500

    - Fix the unit tests

--- test/co/com/application/services/AsynchronousServiceTest.scala
+++ test/co/com/application/services/AsynchronousServiceTest.scala
@@ -1,15 +1,11 @@
 package co.com.application.services
 
-import co.com.domain.services.UserService
-import co.com.factories.UserDTOFactory.LouisUserDTO
-import co.com.factories.UserFactory.LouisUser
 import co.com.tool.FutureTool.waitForFutureResult
 import co.com.{TestKit, TestKitBase}
 import monix.eval.Task
 import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
 
 import scala.concurrent.Future
-import scala.io.StdIn
 import scala.util.Random
 
 class AsynchronousServiceTest extends TestKit with TestKitBase {
@@ -22,24 +18,22 @@ class AsynchronousServiceTest extends TestKit with TestKitBase {
 
           val future = Future.successful( Random.nextInt() )
 
-          val zipFuture: Future[String] = future.zip( future ).map { case ( random1, random2 ) => s"${random1.toString} - ${random2.toString}" }
+          val zipFuture: Future[( Int, Int )] = future.zip( future )
 
           val result = waitForFutureResult( zipFuture )
 
-          result mustBe 10
+          result._1 mustBe result._2
         }
       }
 
       "zip two futures transparent" should {
         "execute future eager" in {
 
-          val future = Future.successful( Random.nextInt() )
-
-          val zipFuture: Future[String] = Future.successful( Random.nextInt() ).zip( Future.successful( Random.nextInt() ) ).map { case ( random1, random2 ) => s"${random1.toString} - ${random2.toString}" }
+          val zipFuture: Future[( Int, Int )] = Future.successful( Random.nextInt() ).zip( Future.successful( Random.nextInt() ) )
 
           val result = waitForFutureResult( zipFuture )
 
-          result mustBe 10
+          assert( result._1 != result._2 )
         }
       }
 
@@ -48,40 +42,35 @@ class AsynchronousServiceTest extends TestKit with TestKitBase {
 
           val task = Task( Random.nextInt() )
 
-          val zipTask = Task.parZip2( task, task ).map { case ( random1, random2 ) => s"${random1.toString} - ${random2.toString}" }
+          val zipTask = Task.parZip2( task, task )
 
           val result = waitForFutureResult( zipTask.runToFuture )
 
-          result mustBe 10
+          assert( result._1 != result._2 )
         }
       }
 
       "zip two task transparent" should {
         "execute task lazy" in {
 
-          val zipTask: Task[String] = Task.parZip2( Task.now( Random.nextInt() ), Task.now( Random.nextInt() ) ).map { case ( random1, random2 ) => s"${random1.toString} - ${random2.toString}" }
+          val zipTask: Task[( Int, Int )] = Task.parZip2( Task.now( Random.nextInt() ), Task.now( Random.nextInt() ) )
 
           val result = waitForFutureResult( zipTask.runToFuture )
 
-          result mustBe 10
+          assert( result._1 != result._2 )
         }
       }
 
       "zip two task output" should {
         "execute task lazy" in {
 
-          val task = Task.now( StdIn.readInt() )
+          val task = Task.now( Random.nextInt() )
 
-          val zipTask = Task.parZip2( task, task ).map { case ( random1, random2 ) => s"${random1.toString} - ${random2.toString}" }
-
-          zipTask.runAsync {
-            case Left( value ) => println( value )
-            case Right( value ) => println( value )
-          }
+          val zipTask = Task.parZip2( task, task )
 
           val result = waitForFutureResult( zipTask.runToFuture )
 
-          result mustBe 10
+          result._1 mustBe result._2
         }
       }
     }
